c5c430ed8386b1937309bbaa5e139ed4c0bb6917
Vitor Basso <vitorbasso93@gmail.com>
2020-05-07 12:19:36 -0300

    init
    calma

---
+++ gerenciador-investimentos/model/src/main/kotlin/com/basso/gerenciadorinvestimentos/GerenciadorInvestimentosApplication.kt
@@ -0,0 +1,14 @@
+package com.basso.gerenciadorinvestimentos.model
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.cloud.openfeign.EnableFeignClients
+
+@SpringBootApplication
+@EnableFeignClients
+class GerenciadorInvestimentosApplication : SpringBootServletInitializer()
+
+fun main(args: Array<String>) {
+	runApplication<GerenciadorInvestimentosApplication>(*args)
+}
