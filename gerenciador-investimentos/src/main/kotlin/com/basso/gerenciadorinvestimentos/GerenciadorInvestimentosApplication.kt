c6ccde7862d182553c12d08fc5a892df84d5033b
Vitor Basso <vitorbasso93@gmail.com>
2020-05-07 11:10:22 -0300

    init
    start.spring.io
    Inicio
    TCC em kotlin + springboot usando maven para gerenciar o projeto

---
+++ gerenciador-investimentos/src/main/kotlin/com/basso/gerenciadorinvestimentos/GerenciadorInvestimentosApplication.kt
@@ -0,0 +1,11 @@
+package com.basso.gerenciadorinvestimentos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GerenciadorInvestimentosApplication
+
+fun main(args: Array<String>) {
+	runApplication<GerenciadorInvestimentosApplication>(*args)
+}
