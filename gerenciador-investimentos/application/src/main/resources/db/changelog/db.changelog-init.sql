70796001b18ae848a3cae085e73591b9c0a73558
Vitor Basso <vitorbasso93@gmail.com>
2020-05-12 12:42:54 -0300

    db
    db start
    liquibase working

---
+++ gerenciador-investimentos/application/src/main/resources/db/changelog/db.changelog-init.sql
@@ -0,0 +1,92 @@
+--liquibase formatted sql
+
+--changeset vitor:1589297069592-1
+CREATE TABLE client (
+    email VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NULL,
+    avatar_image VARCHAR(255) NULL,
+    CONSTRAINT PK_CLIENT PRIMARY KEY (email)
+);
+--rollback DROP TABLE client;
+
+--changeset vitor:1589297069592-2
+CREATE TABLE stock (
+    symbol VARCHAR(6) NOT NULL,
+    current_value DECIMAL(13, 2) NOT NULL,
+    type CHAR(1) NULL,
+    name VARCHAR(255) NULL,
+    corporation VARCHAR(255) NULL,
+    business_area VARCHAR(255) NULL,
+    CONSTRAINT PK_STOCK PRIMARY KEY (symbol)
+);
+--rollback DROP TABLE stock;
+
+--changeset vitor:1589297069592-3
+CREATE TABLE stock_assets (
+    id BIGINT NOT NULL,
+    wallet_id BIGINT NOT NULL,
+    stock_symbol VARCHAR(6) NOT NULL,
+    average_cost DECIMAL(13, 2) NOT NULL,
+    amount INT NOT NULL,
+    CONSTRAINT PK_STOCK_ASSETS PRIMARY KEY (id)
+);
+--rollback DROP TABLE stock_assets;
+
+--changeset vitor:1589297069592-4
+CREATE TABLE user (
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    CONSTRAINT PK_USER PRIMARY KEY (email)
+);
+--rollback DROP TABLE user;
+
+--changeset vitor:1589297069592-5
+CREATE TABLE wallet (
+    id BIGINT NOT NULL,
+    name VARCHAR(255) NULL,
+    broker VARCHAR(255) NULL,
+    loss_daytrade DECIMAL(13, 2) NULL,
+    loss DECIMAL(13, 2) NULL,
+    balance_daytrade DECIMAL(13, 2) NULL,
+    balance DECIMAL(13, 2) NULL,
+    client_email VARCHAR(255) NOT NULL,
+    CONSTRAINT PK_WALLET PRIMARY KEY (id)
+);
+--rollback DROP TABLE wallet;
+
+--changeset vitor:1589297069592-6
+CREATE INDEX client_email ON wallet(client_email);
+--rollback ALTER TABLE wallet DROP INDEX client_email;
+
+--changeset vitor:1589297069592-7
+CREATE INDEX stock_symbol ON stock_assets(stock_symbol);
+--rollback ALTER TABLE stock_assets DROP INDEX stock_symbol;
+
+--changeset vitor:1589297069592-8
+CREATE INDEX wallet_id ON stock_assets(wallet_id);
+--rollback ALTER TABLE stock_assets DROP INDEX wallet_id
+
+--changeset vitor:1589297069592-9
+ALTER TABLE stock_assets
+ADD CONSTRAINT stock_assets_ibfk_1 FOREIGN KEY (wallet_id) REFERENCES wallet (id)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+--rollback ALTER TABLE stock_assets DROP FOREIGN KEY stock_assets_ibfk_1;
+
+--changeset vitor:1589297069592-10
+ALTER TABLE stock_assets
+ADD CONSTRAINT stock_assets_ibfk_2 FOREIGN KEY (stock_symbol) REFERENCES stock (symbol)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+--rollback ALTER TABLE stock_assets DROP FOREIGN KEY stock_assets_ibfk_2;
+
+--changeset vitor:1589297069592-11
+ALTER TABLE user
+ADD CONSTRAINT user_ibfk_1 FOREIGN KEY (email) REFERENCES client (email)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+--rollback ALTER TABLE user DROP FOREIGN KEY user_ibfk_1;
+
+--changeset vitor:1589297069592-12
+ALTER TABLE wallet
+ADD CONSTRAINT wallet_ibfk_1 FOREIGN KEY (client_email) REFERENCES client (email)
+ON UPDATE RESTRICT ON DELETE RESTRICT;
+--rollback ALTER TABLE wallet DROP FOREIGN KEY wallet_ibfk_1;
+
---
+++ init.sql
@@ -0,0 +1,49 @@
+use `gerenciamento_investimento`;
+
+create table if not exists `client`(
+	`email` varchar(255) not null,
+	`name` varchar(255) not null,
+    `avatar_image` varchar(255),
+    primary key (`email`)
+)engine=InnoDB;
+
+create table if not exists `user`(
+	`email` varchar(255) not null,
+    `password` varchar(255) not null,
+    primary key (`email`),
+    foreign key (`email`) references `client` (`email`)
+)engine=InnoDB;
+
+create table if not exists `wallet`(
+	`id` bigint not null,
+	`name` varchar(255),
+    `broker` varchar(255),
+    `loss_daytrade` decimal(13,2),
+    `loss` decimal(13,2),
+    `balance_daytrade` decimal(13,2),
+    `balance` decimal(13,2),
+    `client_email` varchar(255) not null,
+    primary key (`id`),
+    foreign key (`client_email`) references `client` (`email`)
+)engine=InnoDB;
+
+create table if not exists `stock` (
+	`symbol` varchar(6) not null,
+    `current_value` decimal(13,2) not null,
+    `type` char,
+    `name` varchar(255),
+    `corporation` varchar(255),
+    `business_area` varchar(255),
+    primary key (`symbol`)
+)engine=InnoDB;
+
+create table if not exists `stock_assets`(
+	`id` bigint not null,
+    `wallet_id` bigint not null,
+    `stock_symbol` varchar(6) not null,
+    `average_cost` decimal(13,2) not null,
+    `amount` int not null,
+    primary key (`id`),
+    foreign key (`wallet_id`) references `wallet` (`id`),
+    foreign key (`stock_symbol`) references `stock` (`symbol`)
+)engine=InnoDB;
