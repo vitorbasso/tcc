87bb86a722cf3f787083acae498231567e2a6d61
Vitor Basso <vitorbasso93@gmail.com>
2020-05-13 11:21:57 -0300

    modules
    Modulos estabelecidos e primeira implementação vazia realizada - stub

--- gerenciador-investimentos/application/src/main/kotlin/com/basso/gerenciadorinvestimentos/GerenciadorInvestimentosApplication.kt
+++ gerenciador-investimentos/application/src/main/kotlin/com/basso/gerenciadorinvestimentos/GerenciadorInvestimentosApplication.kt
@@ -1,12 +1,16 @@
 package com.basso.gerenciadorinvestimentos
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
-@SpringBootApplication
-@EnableFeignClients
+@SpringBootApplication(scanBasePackages = ["com.basso.gerenciadorinvestimentos"])
+@EntityScan("com.basso.gerenciadorinvestimentos.domain")
+@EnableJpaRepositories("com.basso.gerenciadorinvestimentos.repository")
+@EnableFeignClients("com.basso.gerenciadorinvestimentos.integration")
 class GerenciadorInvestimentosApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
