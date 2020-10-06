109e9557257c7e90e66001eeba52e6cb58c86fd3
Vitor Basso <vitorbasso93@gmail.com>
2020-10-06 20:38:38 -0300

    adjustments

--- gerenciador-investimentos/src/main/kotlin/com/vitorbasso/gerenciadorinvestimentos/integration/YahooApiIntegration.kt
+++ gerenciador-investimentos/src/main/kotlin/com/vitorbasso/gerenciadorinvestimentos/integration/YahooApiIntegration.kt
@@ -1,16 +1,27 @@
 package com.vitorbasso.gerenciadorinvestimentos.integration
 
 import com.vitorbasso.gerenciadorinvestimentos.api.YahooApi
+import com.vitorbasso.gerenciadorinvestimentos.enum.ManagerErrorCode
+import com.vitorbasso.gerenciadorinvestimentos.exception.CustomEntityNotFoundException
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
 @Component
-class YahooApiIntegration (
+class YahooApiIntegration(
     private val yahooApi: YahooApi,
     @Value("\${finance.api.key}")
     private val financeKey: String
-){
+) {
+
+    companion object {
+        const val SYMBOL_SUFFIX = ".SA" //yahoo api necessary for symbols from b3
+    }
+
     fun autoComplete(query: String) = this.yahooApi.searchStock(financeKey, query)
 
-    fun getQuote(symbols: String) = yahooApi.getQuotes(financeKey, "$symbols.SA")
+    fun getQuote(symbols: String)
+        = yahooApi.getQuotes(financeKey, "$symbols$SYMBOL_SUFFIX").quoteResponse.result.firstOrNull()?.let {
+        it.copy(symbol = it.symbol.substringBeforeLast(SYMBOL_SUFFIX))
+    }
+        ?: throw CustomEntityNotFoundException(ManagerErrorCode.MANAGER_03)
 }
--- gerenciador-investimentos/src/main/kotlin/com/vitorbasso/gerenciadorinvestimentos/repository/StockRepository.kt
+++ gerenciador-investimentos/src/main/kotlin/com/vitorbasso/gerenciadorinvestimentos/repository/StockRepository.kt
@@ -1,9 +1,7 @@
 package com.vitorbasso.gerenciadorinvestimentos.repository
 
 import com.vitorbasso.gerenciadorinvestimentos.domain.concrete.Stock
-import com.vitorbasso.gerenciadorinvestimentos.dto.api.response.QuoteDto
-import com.vitorbasso.gerenciadorinvestimentos.enum.ManagerErrorCode
-import com.vitorbasso.gerenciadorinvestimentos.exception.CustomEntityNotFoundException
+import com.vitorbasso.gerenciadorinvestimentos.dto.api.response.Quote
 import com.vitorbasso.gerenciadorinvestimentos.integration.YahooApiIntegration
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
@@ -25,23 +23,21 @@ class StockRepository(
     }
 
     fun findByTicker(ticker: String) = this.dbStockRepository.findByIdOrNull(ticker).let {
-            if(isStockInvalid(it)) this.dbStockRepository.save(this.yahooApiIntegration.getQuote(ticker).getEntity())
-            else it
-        }
+        if (isStockInvalid(it)) this.dbStockRepository.save(this.yahooApiIntegration.getQuote(ticker).getEntity()).also { println("yahoo") }
+        else it.also { println("local") }
+    }
 
     private fun isStockInvalid(stock: Stock?)
         = stock == null || stock.dateUpdated.plusMinutes(STOCK_TTL).isBefore(LocalDateTime.now())
 
 }
 
-private fun QuoteDto.getEntity() = this.quoteResponse.result.firstOrNull()?.let {
-    Stock(
-        ticker = it.symbol.substring(0, it.symbol.length - 3),
-        currentValue = BigDecimal.valueOf(it.regularMarketPrice),
-        openingValue = BigDecimal.valueOf(it.regularMarketOpen),
-        closingValue = BigDecimal.valueOf(it.regularMarketPreviousClose),
-        highestValue = BigDecimal.valueOf(it.regularMarketDayHigh),
-        lowestValue = BigDecimal.valueOf(it.regularMarketDayLow),
-        variation = BigDecimal.valueOf(it.regularMarketChangePercent)
-    )
-} ?: throw CustomEntityNotFoundException(ManagerErrorCode.MANAGER_03)
+private fun Quote.getEntity() = Stock(
+    ticker = this.symbol,
+    currentValue = BigDecimal.valueOf(this.regularMarketPrice),
+    openingValue = BigDecimal.valueOf(this.regularMarketOpen),
+    closingValue = BigDecimal.valueOf(this.regularMarketPreviousClose),
+    highestValue = BigDecimal.valueOf(this.regularMarketDayHigh),
+    lowestValue = BigDecimal.valueOf(this.regularMarketDayLow),
+    variation = BigDecimal.valueOf(this.regularMarketChangePercent)
+)
