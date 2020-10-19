e27ed33bd38dffc68917fd7a8a311be32fc620da
Vitor Basso <vitorbasso93@gmail.com>
2020-10-18 22:45:04 -0300

    Update TransactionController.kt

--- gerenciador-investimentos/src/main/kotlin/com/vitorbasso/gerenciadorinvestimentos/controller/TransactionController.kt
+++ gerenciador-investimentos/src/main/kotlin/com/vitorbasso/gerenciadorinvestimentos/controller/TransactionController.kt
@@ -1,15 +1,12 @@
 package com.vitorbasso.gerenciadorinvestimentos.controller
 
-import com.vitorbasso.gerenciadorinvestimentos.service.concrete.AssetService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/\${api-version}/transactions")
-class TransactionController (
-    private val assetService: AssetService
-) {
+class TransactionController {
 
     @GetMapping
     fun getTransactionsByWallet() {
