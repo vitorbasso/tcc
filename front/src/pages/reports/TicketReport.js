c86548bbb090ba9230c5d201a3866514712d4847
Vitor Basso <vitorbasso93@gmail.com>
2021-10-10 18:23:18 -0300

    bugfix
    - Fixing Variation css and arrow bug in ticket report page

--- front/src/pages/reports/TicketReport.js
+++ front/src/pages/reports/TicketReport.js
@@ -241,8 +241,8 @@ function TicketReport() {
           </div>
           <div>
             <p>Diferença</p>
-            <p className={css}>
-              {arrow}
+            <p className={difCss}>
+              {difArrow}
               {moneyFormatter.format(currentValue - averageValue)}
             </p>
           </div>
